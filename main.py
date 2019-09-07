fc85457ab222121f4b9dca15ad17c72c45e2ee21
Guilherme Luis <guile.hm@hotmail.com>
2019-09-07 17:54:24 -0300

    create authentication method

---
+++ main.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+
+SAMPLE_SPREADSHEET_ID = '14x0KrsstPQ5BG10hGrmMM0Yd-5f4KCA6hgU40dwIQGI'
+SAMPLE_RANGE_NAME = 'Gastos!C4:AL7'
+
+
+def main():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
